ca22b0095c949b6d942b9ca4c82fb6fe873015f5
Tsung-Jen Pu <tsungjen.pu@gmail.com>
2025-05-15 15:18:52 +0200

    adjust Post.jsx and add PostsList.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,15 @@
-function Post() {
+// const names = ["Maximilian", "Manuel"];
+import classes from "./Post.module.css";
+
+function Post(props) {
+  // const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
-    <div>
-      <p>Maximilian Schwarzmueller</p>
-      <p>React.js is awesome!</p>
-    </div>
+    <li className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </li>
   );
 }
-
 
 export default Post;

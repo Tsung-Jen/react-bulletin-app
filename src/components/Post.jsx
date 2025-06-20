375a4e6c18a109d757659bd67c7f04d0c915e63d
Tsung-Jen Pu <tsungjen.pu@gmail.com>
2025-06-20 15:40:09 +0200

    adding dynamic routes for individual post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 // const names = ["Maximilian", "Manuel"];
+import { Link } from "react-router-dom";
 import classes from "./Post.module.css";
 
 function Post(props) {
@@ -6,8 +7,10 @@ function Post(props) {
 
   return (
     <li className={classes.post}>
-      <p className={classes.author}>{props.author}</p>
-      <p className={classes.text}>{props.body}</p>
+      <Link to={props.id}>
+        <p className={classes.author}>{props.author}</p>
+        <p className={classes.text}>{props.body}</p>
+      </Link>
     </li>
   );
 }
